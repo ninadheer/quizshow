d089d170f2fb35cde2d7861cf5b03af396b6119a
Nina Dheer <ninadheer@Nina-Dheers-MacBook-Pro.local>
2014-07-19 14:39:04 -0400

    second changes

--- quizshow.js
+++ quizshow.js
@@ -2,7 +2,21 @@ angular.module('quizApp', [])
   .controller('QuizController', function($scope) {
 	  console.log("Controller called");
 	  $scope.questions = [
-		  "What color is the sky?",
-		  "What color is the grass?"
+		  {
+		  	"question": "What color is the sky?",
+		  	"answers": [
+		  		"blue",
+		  		"yellow",
+		  		"green",
+		  		"purple"]
+		  },
+		  {
+		  	"question": "What time is it?",
+		  	"answers": [
+		  		"10:15",
+		  		"11:00",
+		  		"2:30",
+		  		"2:38"]
+		  }
 	  ];
   });
